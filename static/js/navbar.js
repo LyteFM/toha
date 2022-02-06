457d3dfd015b32cc7f78b026e17dcb3c4d6ead63
Fynn Leitow <19202953+LyteFM@users.noreply.github.com>
2022-02-06 14:09:10 +0100

    :fire: disable navbar change
    and set the initial (and final) state to navbar-light

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,46 +1,11 @@
 "use strict";
 
 const updateNavBar = () => {
-  if ($(document).scrollTop() > 40) {
-    $('#top-navbar').removeClass('initial-navbar');
-    $('#top-navbar').addClass('final-navbar shadow');
-
-    $('#navbar-toggler').removeClass('navbar-dark');
-    $('#navbar-toggler').addClass('navbar-light');
-
-    // get the main logo from hidden img tag
-    let mainLogo = document.getElementById("main-logo")
-    if (mainLogo !== null) {
-      let logoURL = mainLogo.getAttribute("src");
-      $('#logo').attr("src", logoURL);
-    }
-
-  } else {
-    $('#top-navbar').removeClass('final-navbar shadow');
-    $('#top-navbar').addClass('initial-navbar');
-
-    $('#navbar-toggler').removeClass('navbar-light');
-    $('#navbar-toggler').addClass('navbar-dark');
-
-    // get the inverted logo from hidden img tag
-    let invertedLogo = document.getElementById("inverted-logo")
-    if (invertedLogo !== null) {
-      let logoURL = invertedLogo.getAttribute("src");
-      $('#logo').attr("src", logoURL);
-    }
-  }
+ console.log("Doin' Nothin'...")
 };
 
 (function ($) {
   jQuery(document).ready(function () {
-
-    // change navbar style on scroll
-    // ==================================================
-    // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-    // $.onscroll = function() {scrollFunction()};
-    $(document).scroll(function () {
-      updateNavBar();
-    });
 
     // Creates a click handler to collapse the navigation when
     // anchors in the mobile nav pop up are clicked
@@ -51,7 +16,6 @@ const updateNavBar = () => {
       });
     }
 
-    updateNavBar();
   });
 
 })(jQuery);
